2c9b6a23b17ceafa1bc5345d1c0bf5e9349088e4
Nazmulnafim <97496403+Nazmulnafim@users.noreply.github.com>
2023-12-08 16:24:09 +0600

    Update new.py

--- new.py
+++ new.py
@@ -2,68 +2,12 @@ import streamlit as st
 from tensorflow.keras import models
 import numpy as np
 from PIL import Image
-import cv2
 
 # Load the pre-trained model
 loaded_model = models.load_model("model_mobilenetv2_densenet201_9941_acc.hdf5")
 
 def welcome():
-    st.write("Hello World")
-
-def capture_image():
-    # Capture an image using the webcam
-    video_capture = cv2.VideoCapture(0)
-
-    if not video_capture.isOpened():
-        st.write("Failed to open webcam")
-        return
-
-    # Capture and display frames until the user presses ESC
-    while True:
-        success, frame = video_capture.read()
-
-        if not success:
-            break
-
-        cv2.imshow('Webcam', frame)
-
-        key = cv2.waitKey(1) & 0xFF
-        if key == 27: # ESC key to quit
-            break
-
-    video_capture.release()
-
-    # Convert the captured image to RGB format and resize it
-    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    resized_image = cv2.resize(rgb_image, (224, 224))
-
-    # Convert the image to a NumPy array
-    image_array = np.array(resized_image)
-
-    # Normalize the image pixel values
-    image_array = image_array / 255
-
-    # Add a batch dimension
-    image_array = np.expand_dims(image_array, axis=0)
-
-    # Make predictions using the model
-    preds = loaded_model.predict(image_array)
-    class_idx = np.argmax(preds, axis=1)[0]
-    confidence = round(float(preds[0][class_idx]), 3)
-
-    class_labels = [
-        "The leaf is diseased cotton leaf. The disease is Aphids",
-        "The leaf is diseased cotton plant. The disease is Army worm",
-        "The leaf is diseased cotton leaf. The disease is Bacterial blight",
-        "The leaf is Healthy.",
-        "Powdery mildew",
-        "The leaf is diseased cotton plant. The disease is Target Spot",
-        "The leaf is diseased cotton plant. The disease is Curl Virus",
-        "The leaf is diseased cotton plant. The disease is Fussium Wilt"
-    ]
-    predicted_class = class_labels[class_idx]
-
-    return predicted_class, confidence
+    st.write("Cotton Leaf Disease Classification")
 
 def handle_request():
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
@@ -100,7 +44,6 @@ def handle_request():
             'confidence': confidence
         })
 
-    # Capture
 # Run the Streamlit app
 if __name__ == '__main__':
     welcome()
